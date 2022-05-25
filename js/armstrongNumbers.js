4d5505da8af48ff09cd65d84f88a9724e7153a79
nhungbi <nhungvan16@gmail.com>
2022-05-24 21:49:18 -0500

    need unit

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -4,11 +4,13 @@ exports.findArmstrongNumbers = function(arrays) {
     armstrongArray = []
     for (let num of arrays) {
         const num_digit = String(num).length
-        const lDigString= String(num).split('')
+        const lDigString= String(num).split('') //array of string of digit
         const digitsArray = lDigString.filter(function (digit) {
-            return Number(digit)})
+            return Number(digit)}) //array of number digits
+
         const totalArray = digitsArray.map( x => x**num_digit)
-        sum = totalArray.reduce((prev,next) => prev + next, 0 )
+        sum = totalArray.reduce((prev,next) => prev + next, 0 ) //sum all of the digits to the power of digit num
+
         if (sum === num) {
             armstrongArray.push(num)
         }
@@ -16,4 +18,4 @@ exports.findArmstrongNumbers = function(arrays) {
     return armstrongArray
 
 };
-console.log(exports.findArmstrongNumbers([1]))
+//console.log(exports.findArmstrongNumbers([1]))
